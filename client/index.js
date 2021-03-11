56e1fb2e4a6fc1abce9ec4d201da3ba661f02f66
MelindaJoArmbruster <melinda@armbrustermail.com>
2021-03-11 15:22:44 -0600

    stripe working

--- client/index.js
+++ client/index.js
@@ -5,15 +5,23 @@ import {Router} from 'react-router-dom'
 import history from './history'
 import store from './store'
 import App from './app'
+import {loadStripe} from '@stripe/stripe-js'
+import {Elements} from '@stripe/react-stripe-js'
+
+const promise = loadStripe(
+  'pk_test_51ITC4REclLCTO1i8Ii7KMFc5Hhh7q8L9goymmiQkkiibIR2qUqh8OdM8ATmRoNC4NCGa7G4MBs3ZKN7lPqRQ7lFR00MRhDCusb'
+)
 
 // establishes socket connection
 import './socket'
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={history}>
-      <App />
-    </Router>
+    <Elements stripe={promise}>
+      <Router history={history}>
+        <App />
+      </Router>
+    </Elements>
   </Provider>,
   document.getElementById('app')
 )
