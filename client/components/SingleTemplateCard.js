aae1047b887d9208c842c41111414290ad53fbe9
MelindaJoArmbruster <melinda@armbrustermail.com>
2021-03-13 08:36:28 -0600

    Initial Album View Complete

--- client/components/SingleTemplateCard.js
+++ client/components/SingleTemplateCard.js
@@ -3,18 +3,52 @@ import {Link} from 'react-router-dom'
 
 const SingleTemplateCard = props => {
   return (
-    <div>
-      <div className="templateCard">
+    <div className="col">
+      <div className="card h-100 shadow-sm">
         <img
-          className="singleTemplateImage"
+          className="bd-placeholder-img card-img-top"
+          width="100%"
+          height="225"
           src={props.template.frontImageURL}
+          aria-label="Placeholder: Thumbnail"
+          preserveAspectRatio="xMidYMid slice"
+          focusable="false"
         />
-        <Link to={`/templates/${props.template.id}`}>
-          {props.template.name}
-        </Link>
+
+        <div className="card-body">
+          <h5 className="card-title">{props.template.name}</h5>
+          <p className="card-text">{props.template.description}</p>
+          <div className="d-flex justify-content-between align-items-center">
+            <div className="btn-group">
+              <Link to={`/templates/${props.template.id}`}>
+                <button
+                  type="button"
+                  className="btn btn-sm btn-outline-secondary"
+                >
+                  View
+                </button>
+              </Link>
+            </div>
+            <small className="text-muted">$2.99</small>
+          </div>
+        </div>
       </div>
     </div>
   )
+
+  // return (
+  //   <div>
+  //     <div className="templateCard">
+  //       <img
+  //         className="singleTemplateImage"
+  //         src={props.template.frontImageURL}
+  //       />
+  //       <Link to={`/templates/${props.template.id}`}>
+  //         {props.template.name}
+  //       </Link>
+  //     </div>
+  //   </div>
+  // )
 }
 
 export default SingleTemplateCard
