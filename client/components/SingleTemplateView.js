24027c1e09ca6602579fe41939e7e0540ea6b9b5
MelindaJoArmbruster <melinda@armbrustermail.com>
2021-03-14 08:42:11 -0500

    Log in to Personalize Logic

--- client/components/SingleTemplateView.js
+++ client/components/SingleTemplateView.js
@@ -36,16 +36,28 @@ class SingleTemplateView extends React.Component {
           />
         </div>
         <div className="linkDiv">
-          <Link
-            to={{
-              pathname: '/order',
-              templateId: this.props.singleTemplate.id
-            }}
-          >
-            <button className="btn btn-sm btn-outline-secondary m-3">
-              Personalize This Card
-            </button>
-          </Link>
+          {this.props.isLoggedIn ? (
+            <Link
+              to={{
+                pathname: '/order',
+                templateId: this.props.singleTemplate.id
+              }}
+            >
+              <button className="btn btn-sm btn-outline-secondary m-3">
+                Personalize This Card
+              </button>
+            </Link>
+          ) : (
+            <Link
+              to={{
+                pathname: '/login'
+              }}
+            >
+              <button className="btn btn-sm btn-outline-secondary m-3">
+                Log In to Personalize
+              </button>
+            </Link>
+          )}
           <Link to={{pathname: '/templates'}}>
             <button className="btn btn-sm btn-outline-secondary m-3">
               Back to All Templates
@@ -60,7 +72,8 @@ class SingleTemplateView extends React.Component {
 const mapStateToProps = state => {
   return {
     singleTemplate: state.singleTemplateSlice.singleTemplate,
-    loading: state.singleTemplateSlice.loading
+    loading: state.singleTemplateSlice.loading,
+    isLoggedIn: !!state.user.id
   }
 }
 
